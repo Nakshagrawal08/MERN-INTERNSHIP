b7b7441c97a58b5d883985035196e66f9f9f5eb5
Nakshagrawal08 <kanhaagrawal080604@gmail.com>
2025-02-19 16:19:48 +0530

    changes folder

--- quiz/ouizprocess.js
+++ quiz/ouizprocess.js
@@ -218,7 +218,7 @@ var datas=[]
         // quiz one by one
 
 
-var index
+        var index
         function showQuiz(index) {
             
             var result=document.getElementById('box');
