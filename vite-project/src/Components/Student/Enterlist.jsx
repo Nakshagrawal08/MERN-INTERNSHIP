0f13befbcc1d76d9451aade9afbd9b338bf27ef7
nakshagrawal08 <kanhaagrawal080604@gmail.com>
2025-03-01 16:21:48 +0530

    todo

--- vite-project/src/Components/Student/Enterlist.jsx
+++ vite-project/src/Components/Student/Enterlist.jsx
@@ -3,8 +3,9 @@ import StudentList from './studentList'
 
 function Enterlist(prop) {
   return (
-    <li>
-{prop.name} {prop.surname}{prop.age}
+    <li className=' p-3 bg-amber-200 justify-between border-black border-2 flex  text-black m-3'>
+      {prop.index+1}. {prop.name}  <div/> {prop.surname}  <div/>  {prop.age}
+      <div/> <button onClick={()=>prop.Deletedetail(prop.index)} className='bg-blue-400 ml-3 hover:bg-blue-500 p-2 rounded-2xl'>delete</button> {/* lifting state up */}
     </li>
   )
 }
--- vite-project/src/Components/Student/login/Pagestatus.jsx
+++ vite-project/src/Components/Student/login/Pagestatus.jsx
@@ -1,13 +1,15 @@
 import React,{useState} from 'react'
 import Home from './Home'
-import login from './login'
+import Login from './login'
+
 function Pagestatus() {
 
     const [isLogin,SetisLogin]=useState(false) 
   return (
     <div>
-        <button onClick={()=>{SetisLogin(!isLogin)}} className='p-2 bg-blue-300 hover:bg-blue-400'>change</button><br/>
       {isLogin?<Home/>:<Login/>}
+        <button onClick={()=>SetisLogin(!isLogin)} className='p-2 bg-blue-300 hover:bg-blue-400'>change</button><br/>
+      
     </div>
   )
   
