b4f5bd328e0abb0545351a7e0553355de9f32099
Nakshagrawal08 <kanhaagrawal080604@gmail.com>
2025-03-06 01:00:16 +0530

    User

--- vite-project/src/Components/Axios/User.jsx
+++ vite-project/src/Components/Axios/User.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import Selected from './Selected'
 export default function user() {
     const [ListOfUsers,SetListOfUser]=useState([])
+    const [Option , SetOption] = useState('')
     useEffect ( ()=>{
         async function Getdata() {
             let Response =await axios.get('https://jsonplaceholder.typicode.com/users')
@@ -14,15 +16,21 @@ export default function user() {
         Getdata()
         
     },[])
+    const Getdetails = (id)=>{
+      SetOption(id)
+     
+    }
   return (
-    <div>
-      {
+    <div className='flex'>
+      <div>{
    
       ListOfUsers.map((user)=>
-        <h1 className='text-black'>id: {user.id}</h1>
+        <h1 onClick={()=>Getdetails(user.id)}  className='text-black p-4 border-black border-1 m-2 w-72 hover:font-bold'> id: {user.id}<br/> Name :{ user.name}</h1>
       )
       }
-    </div>
+      </div>
+        <Selected Details ={ListOfUsers} Selected={Option} />
+      </div>
   )
 }
 
