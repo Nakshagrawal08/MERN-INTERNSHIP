7233481f28485725b0d7d527fd272202b45f29b8
nakshagrawal08 <kanhaagrawal080604@gmail.com>
2025-04-02 13:41:40 +0530

    post

--- ReactProjects/SocialMediaApp/src/Components/Exceptuser.jsx
+++ ReactProjects/SocialMediaApp/src/Components/Exceptuser.jsx
@@ -4,20 +4,26 @@ import linkedin from './linkedin.png'
 import axios from 'axios'
 function Exceptuser() {
 
-  const [ListOfUsers,SetListOfUser]=useState([])
+  const [ListOfUser,SetListOfUser]=useState([])
+  const [ListOfPosts,SetListOfPost]=useState([])
     const [Option , SetOption] = useState('')
     useEffect ( ()=>{
         async function Getdata() {
             let Response =await axios.get('https://jsonplaceholder.typicode.com/users')
             if(Response.status==200){
                 SetListOfUser(Response.data)
-            }
-            
+            }   
+        };
+        async function Getposts() {
+          let Response =await axios.get('https://jsonplaceholder.typicode.com/posts')
+            if(Response.status==200){
+                SetListOfPost(Response.data)
+            }   
         }
         Getdata()
-        
+        Getposts()
     },[])
-
+  
    
  
     const Getdetails = (id)=>{
@@ -29,14 +35,15 @@ function Exceptuser() {
     <>
      
 
-        {ListOfUsers.map((user)=>
+        {ListOfPosts.map((post)=>ListOfUser.map((user)=>
         <div className=' flex  items-center space-x-4 hover:px-4 hover:ease-in-out hover:duration-400 bg-gray-100 hover:bg-gray-200 p-2 rounded-lg shadow-md'>
         <img src={linkedin} className=' shadow-2xs  rounded-full size-13 '></img>
         
           <h1 onClick={()=>Getdetails(user.id)}  className='text-black p-4  m-2 w-72 hover:font-bold'>{ user.name}</h1>
+          <p className="text-blue-500 text-sm cursor-pointer">{user.id==post.userId?"post":"no post"}</p>
           <p className="text-blue-500 text-sm cursor-pointer">View More...</p>
             
-            </div>)
+            </div>))
         }
         
     </>
